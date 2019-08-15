352c0df3778bba99839171acadff5977f8563526
Philipp Ossler <philipp.ossler@gmail.com>
2019-08-16 01:47:01 +0200

    select also cat cards to play

--- src/main/java/io/zeebe/casino/user/SelectAction.java
+++ src/main/java/io/zeebe/casino/user/SelectAction.java
@@ -3,9 +3,11 @@ package io.zeebe.casino.user;
 import io.zeebe.client.api.response.ActivatedJob;
 import io.zeebe.client.api.worker.JobClient;
 import io.zeebe.client.api.worker.JobHandler;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.Random;
+import java.util.Optional;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Collectors;
 import org.slf4j.Logger;
@@ -31,62 +33,61 @@ public class SelectAction implements JobHandler {
     {
       log.info("Player {} passes.", nextPlayer);
 
-      jobClient.newCompleteCommand(activatedJob.getKey()).variables(Map.of("action", "")).send();
+      jobClient.newCompleteCommand(activatedJob.getKey()).variables(Map.of(
+          "cards", Collections.emptyList(),
+          "action", "pass")).send();
     }
     else
     {
       final Map players = (Map) variables.get("players");
-
       final List<String> hand = (List<String>) players.get(nextPlayer);
-// CAT algo ?
-//      final Map<String, List<String>> cardGroups = hand.stream()
-//          .filter(card -> card.contains("cat"))
-//          .collect(Collectors.groupingBy(card -> card));
-//
-//
-//      if (cardGroups.size() > 2)
-//      {
-//        if (cardGroups.containsKey("feral-cat"))
-//        {
-//
-//          final int feralCount = cardGroups.get("feral-cat").size();
-//
-//        }
-//      }
-//      else
-//      {
-//        //
-//      }
-//
-//
-//
-//      final long catCount = hand.stream().filter(card -> card.contains("cat")).count();
 
-      final List<String> handWithoutDefuse = hand.stream().filter(card -> !card.equals("defuse") && !card.equals("nope"))
-//          .filter(card -> catCount >= 2 ? true : !card.contains("cat"))
+      final Map<String, List<String>> catCards =
+          hand.stream()
+              .filter(card -> card.contains("cat"))
+              .collect(Collectors.groupingBy(card -> card));
+
+      final var twoSameCatCards = catCards
+          .entrySet()
+          .stream()
+          .filter(e -> e.getValue().size() >= 2)
+          .map(e -> e.getValue().subList(0, 2))
+          .findFirst();
+
+      final var catCard = catCards.keySet().stream().filter(card -> !card.equals("feral-cat"))
+          .findFirst();
+
+      final var feralAndOtherCat = Optional.of(catCards)
+          .filter(cards -> cards.size() >= 2)
+          .filter(cards -> cards.containsKey("feral-cat"))
+          .map(cards -> List.of("feral-cat", catCard.get()));
+
+      final var actionCards = hand
+          .stream()
+          .filter(card -> !List.of("nope", "defuse").contains(card))
+          .filter(card -> !card.contains("cat"))
           .collect(Collectors.toList());
 
-      final int handSize = handWithoutDefuse.size();
+      final var actionCard = Optional.of(actionCards)
+          .filter(cards -> !cards.isEmpty())
+          .map(cards -> {
+            final var cardIdx = ThreadLocalRandom.current().nextInt(0, cards.size());
+            return List.of(cards.get(cardIdx));
+          });
 
-      if (handSize == 0)
-      {
-        // pass
-        jobClient.newCompleteCommand(activatedJob.getKey()).variables(Map.of("action", "")).send();
-        return;
-      }
+      final Map<String, Object> cards = twoSameCatCards
+          .or(() -> feralAndOtherCat)
+          .or(() -> actionCard)
+          .map(cardsToPlay -> Map.of(
+              "cards", cardsToPlay,
+              "action", cardsToPlay.get(0)))
+          .orElse(Map.of(
+              "cards", Collections.emptyList(),
+              "action", "pass"));
 
-      final int index = ThreadLocalRandom.current().nextInt(0, handSize);
-      final String card = handWithoutDefuse.remove(index);
-      hand.remove(card);
+      log.info("Player {} picked card(s) '{}' to play", nextPlayer, cards.get("cards"));
 
-      log.info("Player {} picked card {} to play", nextPlayer, card);
-
-      players.put(nextPlayer, hand);
-      jobClient.newCompleteCommand(activatedJob.getKey()).variables(Map.of(
-          "players", players,
-          "cards", List.of(card),
-          "action", card
-      )).send();
+      jobClient.newCompleteCommand(activatedJob.getKey()).variables(cards).send();
     }
   }
 }
