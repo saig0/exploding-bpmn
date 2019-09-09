af96bfa94343dc9e1ad0cf4e71ec6982b6ba58ce
Christopher Zell <zelldon91@googlemail.com>
2019-09-09 13:02:43 +0200

    make app cloud ready

--- slack/src/main/java/io/zeebe/bpmn/games/slack/ZeebeConfig.java
+++ slack/src/main/java/io/zeebe/bpmn/games/slack/ZeebeConfig.java
@@ -1,21 +1,41 @@
 package io.zeebe.bpmn.games.slack;
 
 import io.zeebe.client.ZeebeClient;
+import io.zeebe.client.impl.OAuthCredentialsProvider;
+import io.zeebe.client.impl.OAuthCredentialsProviderBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ZeebeConfig {
+  private static final Logger LOG = LoggerFactory.getLogger(ZeebeConfig.class);
 
-  @Value("${zeebe.brokerContactPoint:localhost:26500}")
-  private String brokerContactPoint;
+  @Value("${cloud.clientId}")
+  private String cloudClientId;
+
+  @Value("${cloud.clientSecret}")
+  private String cloudClientSecret;
+
+  @Value("${cloud.authServer:https://login.cloud.ultrawombat.com/oauth/token}")
+  private String cloudAuthServer;
+
+  @Value("${cloud.contactPoint}")
+  private String contactPoint;
 
   @Bean
   public ZeebeClient zeebeClient() {
+    LOG.info("Configuration: \n Client id: {}, \n Client secret: {}, \n Auth server: {}, \n Contactpoint: {}", cloudClientId, cloudClientSecret, cloudAuthServer, contactPoint);
+
+    final OAuthCredentialsProviderBuilder c = new OAuthCredentialsProviderBuilder();
+    final OAuthCredentialsProvider cred = c.audience(contactPoint).clientId(cloudClientId)
+        .clientSecret(cloudClientSecret).authorizationServerUrl(cloudAuthServer).build();
+
     return ZeebeClient.newClientBuilder()
-        .brokerContactPoint(brokerContactPoint)
-        .usePlaintext()
+        .brokerContactPoint(contactPoint + ":443")
+        .credentialsProvider(cred)
         .build();
   }
 }
