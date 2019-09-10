60d01cc5934d0298e4c1d38e617f05e91e261324
Christopher Zell <zelldon91@googlemail.com>
2019-09-10 14:53:02 +0200

    add get resource

--- slack/src/main/java/io/zeebe/bpmn/games/slack/SlashCommands.java
+++ slack/src/main/java/io/zeebe/bpmn/games/slack/SlashCommands.java
@@ -18,6 +18,7 @@ import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -42,6 +43,12 @@ public class SlashCommands {
   @Autowired private GamesApplication gamesApplication;
 
   @Autowired private SlackSession session;
+
+  @GetMapping("/hello")
+  public String helloSlackApp()
+  {
+    return "Hello user!";
+  }
 
   @PostMapping("/new-game")
   public SlashCommandResponse newGame(@RequestBody String body) {
