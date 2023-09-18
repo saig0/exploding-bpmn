94f8d0b99d54b631ee106a618d6f3243c101e13b
Philipp Ossler <philipp.ossler@gmail.com>
2023-09-18 09:14:51 +0200

    fix: Deploy resources only once

--- src/main/java/io/zeebe/bpmn/games/GameApplication.java
+++ src/main/java/io/zeebe/bpmn/games/GameApplication.java
@@ -1,13 +1,13 @@
 package io.zeebe.bpmn.games;
 
 import io.camunda.zeebe.spring.client.EnableZeebeClient;
-import io.camunda.zeebe.spring.client.annotation.ZeebeDeployment;
+import io.camunda.zeebe.spring.client.annotation.Deployment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @EnableZeebeClient
 @SpringBootApplication
-@ZeebeDeployment(resources = {"classpath*:/**/*.bpmn", "classpath*:/**/*.dmn"})
+@Deployment(resources = {"classpath*:/*.bpmn", "classpath*:/bot/*.dmn"})
 public class GameApplication {
 
   public static void main(String[] args) {
